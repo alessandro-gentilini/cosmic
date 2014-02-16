56a40a66f2dfbfb22dc828cf52a9e5100039d1de
Alessandro Gentilini <alessandro.gentilini@gmail.com>
2014-02-16 15:17:44 +0100

    added data graphs

--- events.R
+++ events.R
@@ -22,6 +22,11 @@ generate_graph <- function(filename)
   df$timestamp = to_POSIXct(df$timestamp)
   df$threshold <- df$mean+10*df$sd
   
+  n=nrow(df[df$maximum > df$threshold,])
+  N=nrow(df)
+  dt=difftime(df$timestamp[N],df$timestamp[1],unit="secs")
+  title = sprintf("%s, %d events in %s",as.Date(df$timestamp[1]),n,format(.POSIXct(dt,tz="GMT"), "%H:%M:%S"))
+  
   df2=df
   df2$minimum <- NULL
   df2$q <- NULL
@@ -31,7 +36,7 @@ generate_graph <- function(filename)
   df2 <- melt(df2 ,  id = 'timestamp', variable_name = 'measurement')
   plot(ggplot(df2, aes(timestamp,value)) + geom_line(aes(colour = measurement))+
          ylab("pixel grey value")+
-         ggtitle(as.Date(df$timestamp[1]))+
+         ggtitle(title)+
          scale_x_datetime(breaks = "1 hour", minor_breaks="15 min",labels = date_format("%H")) + 
          scale_color_hue(breaks=c("maximum","mean","threshold"),label=c(expression(M[i]),expression(bar(M)[i]),expression(bar(M)[i]+10%.%sigma[M][i])))
   )
@@ -47,5 +52,13 @@ generate_graph <- function(filename)
 }
 
 op <- options(digits.secs=3)
+#generate_graph("stats8.txt")
+generate_graph("stats9.txt")
+generate_graph("stats10.txt")
+#generate_graph("stats12.txt")
+generate_graph("stats13.txt")
+generate_graph("stats14.txt")
+generate_graph("stats15.txt")
 generate_graph("stats16.txt")
 generate_graph("stats17.txt")
+#generate_graph("short.txt")
